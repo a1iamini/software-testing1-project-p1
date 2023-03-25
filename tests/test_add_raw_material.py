07b3d0f002469fbeb38102e81f9d260e716552bd
a1iamini <aliaministu@gmail.com>
2023-03-26 01:22:19 +0330

    test: the wrong add command
    test if the wrong add command is entered then the invalid command message must be printed on the output

--- tests/test_add_raw_material.py
+++ tests/test_add_raw_material.py
@@ -1,3 +1,5 @@
+import pytest
+
 import app
 from confectionary import stockroom
 from unittest.mock import patch, MagicMock
@@ -15,3 +17,27 @@ def test_if_the_raw_material_addition_was_successful_then_a_done_message_must_be
     app.runner()
     out, err = capsys.readouterr()
     assert out == label + "Done\n"
+
+
+@pytest.mark.parametrize('command',
+                         [
+                             "add",
+                             "addhahg",
+                             "add shekar",
+                             "add shekar dd",
+                             "add 44 43",
+                             "add shekar -1",
+                             "add shekar 1.5",
+                             "add shekar 45 345",
+                             "add shekar mekar 345",
+                             "add.shekar.4",
+                             "              ",
+                             ""
+                         ])
+def test_if_the_wrong_add_command_is_entered_then_the_invalid_command_message_must_be_printed_on_the_output(capsys,
+                                                                                                            command):
+    label = "cms: "
+    with patch('app.get_command', MagicMock(return_value=command)):
+        app.runner()
+        out, err = capsys.readouterr()
+        assert out == label + "invalid command\n"
