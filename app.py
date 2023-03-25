2e6334fcff6290e3cf6ca023f3c82e4e229a35bb
a1iamini <aliaministu@gmail.com>
2023-03-26 02:26:17 +0330

    feat: program termination
    if the exit command is entered then the program must be terminate

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ def runner():
     command = get_command()
     if command.startswith("add ") and add(command):
         print_message("Done")
+    elif command == "exit":
+        return False
     else:
         print_message("invalid command")
+    return True
 
 
 def main():
     keep_gonna = True
     while keep_gonna:
-        runner()
+        if not runner():
+            keep_gonna = False
+    exit(0)
 
 
 if __name__ == "__main__":
