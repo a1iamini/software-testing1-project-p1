a52567f73f4163da66a19d94bc896b8e941a4482
a1iamini <aliaministu@gmail.com>
2023-03-25 20:39:26 +0330

    test: print appropriate message when raw materials are added
    if the raw material addition was successful then a done message must be printed on the output

--- tests/test_add_raw_material.py
+++ tests/test_add_raw_material.py
@@ -8,3 +8,10 @@ def test_add_raw_material_to_stockroom_with_command():
     app.runner()
     assert 'shekar' in stockroom.RAW_MATERIALS and stockroom.RAW_MATERIALS['shekar'] == 10
 
+
+@patch('app.get_command', MagicMock(return_value="add shekar 10"))
+def test_if_the_raw_material_addition_was_successful_then_a_done_message_must_be_printed_on_the_output(capsys):
+    label = "cms: "
+    app.runner()
+    out, err = capsys.readouterr()
+    assert out == label + "Done\n"
