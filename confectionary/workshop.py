6bd2958c2f41cd81515870054c4448af6cc894c2
a1iamini <aliaministu@gmail.com>
2023-03-27 00:38:31 +0330

    feat: define sweets for second or more times
    if sweets is defined for second or more times then the last definition should be considered

--- confectionary/workshop.py
+++ confectionary/workshop.py
@@ -4,6 +4,8 @@ SWEETS_SPEC = dict()
 def define_sweets_spec(sweets_name, materials):
     global SWEETS_SPEC
     materials_list = materials.split(', ')
+    if sweets_name in SWEETS_SPEC:
+        del SWEETS_SPEC[sweets_name]
     for m in materials_list:
         material, quantity = m.split()
         try:
