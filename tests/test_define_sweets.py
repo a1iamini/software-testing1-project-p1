3133be084f5bb8e7338187511efa779c47a98d56
a1iamini <aliaministu@gmail.com>
2023-03-27 00:34:08 +0330

    test: define sweets for second or more times
    test if sweets is defined for second or more times then the last definition should be considered

--- tests/test_define_sweets.py
+++ tests/test_define_sweets.py
@@ -1,3 +1,5 @@
+import pytest
+
 import app
 from unittest.mock import patch, MagicMock
 from confectionary import store
@@ -24,3 +26,25 @@ def test_if_sweets_is_defined_then_its_raw_material_specification_must_be_added_
         assert 'khamei' in workshop.SWEETS_SPEC \
                and workshop.SWEETS_SPEC['khamei']['khame'] == 10 \
                and workshop.SWEETS_SPEC['khamei']['shekar'] == 5
+
+
+@pytest.fixture()
+def define_more():
+    commands = [
+        "define sweets khamei 20000: khame 10, shekar 5",
+        "define sweets khamei 10000: khame 12",
+        "define sweets khamei 30000: khame 10, shekar 5",
+        "define sweets khamei 70000: khame 3, ghand 3",
+    ]
+    for c in commands:
+        with patch('app.get_command', MagicMock(return_value=c)):
+            app.runner()
+
+
+def test_if_sweets_is_defined_for_second_or_more_times_then_the_last_definition_should_be_considered(define_more):
+    assert 'khamei' in workshop.SWEETS_SPEC \
+           and workshop.SWEETS_SPEC['khamei']['khame'] == 3 \
+           and workshop.SWEETS_SPEC['khamei']['ghand'] == 3 \
+           and "shekar" not in workshop.SWEETS_SPEC['khamei'] \
+           and "khamei" in store.SWEETS \
+           and store.SWEETS["khamei"] == 70000
