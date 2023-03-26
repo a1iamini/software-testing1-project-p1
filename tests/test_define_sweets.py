67154106c59e442751f9b3105fda3a46314ea0d1
a1iamini <aliaministu@gmail.com>
2023-03-26 17:10:34 +0330

    test: print appropiate message when sweets are defined
    test if the sweets definition was successful then a done message must be printed on the output

--- tests/test_define_sweets.py
+++ tests/test_define_sweets.py
@@ -7,3 +7,11 @@ def test_if_sweets_is_defined_then_its_name_and_price_must_be_added_to_the_store
     with patch('app.get_command', MagicMock(return_value="define sweets khamei 10000: khame 10, shekar 5")):
         app.runner()
         assert "khamei" in store.SWEETS and store.SWEETS["khamei"] == 10000
+
+
+@patch('app.get_command', MagicMock(return_value="define sweets khamei 10000: khame 10, shekar 5"))
+def test_if_the_sweets_definition_was_successful_then_a_done_message_must_be_printed_on_the_output(capsys):
+    label = "cms: "
+    app.runner()
+    out, err = capsys.readouterr()
+    assert out == label + "Done\n"
