5de2844bbd8f37e14d4991b65ea819ebdf321042
a1iamini <aliaministu@gmail.com>
2023-03-26 21:16:04 +0330

    test: save sweets specification in the workshop
    test if sweets is defined then its raw material spec must be added to the workshop

--- tests/test_define_sweets.py
+++ tests/test_define_sweets.py
@@ -1,6 +1,7 @@
 import app
 from unittest.mock import patch, MagicMock
 from confectionary import store
+from confectionary import workshop
 
 
 def test_if_sweets_is_defined_then_its_name_and_price_must_be_added_to_the_store():
@@ -15,3 +16,11 @@ def test_if_the_sweets_definition_was_successful_then_a_done_message_must_be_pri
     app.runner()
     out, err = capsys.readouterr()
     assert out == label + "Done\n"
+
+
+def test_if_sweets_is_defined_then_its_raw_material_specification_must_be_added_to_the_workshop():
+    with patch('app.get_command', MagicMock(return_value="define sweets khamei 10000: khame 10, shekar 5")):
+        app.runner()
+        assert 'khamei' in workshop.SWEETS_SPEC \
+               and workshop.SWEETS_SPEC['khamei']['khame'] == 10 \
+               and workshop.SWEETS_SPEC['khamei']['shekar'] == 5
